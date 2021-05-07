51d38875b74dd0ef4664689b1eb972d3cda8f3bb
Ryandgs <ryandouglasdasilva@outlook.com>
2021-05-06 22:45:01 -0300

    Migration for Costumers Products & Orders

---
+++ database/migrations/2021_05_07_002340_create_orders_product_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders_products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id');
+            $table->foreign('order_id')->references('id')->on('orders')->contrained();
+            $table->foreignId('product_id');
+            $table->foreign('product_id')->references('id')->on('products')->contrained();
+            $table->decimal('qtt', 10, 3);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders_product');
+    }
+}
