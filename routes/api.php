649c2bcc548f3d841d0e592bf15e0c0c94296385
Ryandgs <ryandouglasdasilva@outlook.com>
2021-05-06 22:46:26 -0300

    Define routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\CostumersController;
+use App\Http\Controllers\OrdersController;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\OrdersProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +20,32 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('clientes')->group(function () {
+    Route::get('/', [CostumersController::class, 'index']);
+    Route::get('/{id}', [CostumersController::class, 'show']);
+    Route::post('/', [CostumersController::class, 'store']);
+    Route::put('/{id}', [CostumersController::class, 'update']);
+    Route::delete('/{id}', [CostumersController::class, 'destroy']);
+});
+
+Route::prefix('produtos')->group(function () {
+    Route::get('/', [ProductsController::class, 'index']);
+    Route::get('/{id}', [ProductsController::class, 'show']);
+    Route::post('/', [ProductsController::class, 'store']);
+    Route::put('/{id}', [ProductsController::class, 'update']);
+    Route::delete('/{id}', [ProductsController::class, 'destroy']);
+});
+
+Route::prefix('pedidos')->group(function () {
+    Route::get('/', [OrdersController::class, 'index']);
+    Route::get('/{id}', [OrdersController::class, 'show']);
+    Route::post('/', [OrdersController::class, 'store']);
+    Route::put('/{id}', [OrdersController::class, 'update']);
+    Route::delete('/{id}', [OrdersController::class, 'destroy']);
+
+    Route::prefix('produtos')->group(function () {
+        Route::post('/{id}', [OrdersProductController::class, 'store']);
+    });
 });
