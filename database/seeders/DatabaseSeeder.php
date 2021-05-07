d4075b1fd415943553659a3814f10d9a914aab09
Ryandgs <ryandouglasdasilva@outlook.com>
2021-05-06 22:46:14 -0300

    Seeder config

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+        \App\Models\Costumers::factory(10)->create();
+        \App\Models\Products::factory(50)->create();
+        \App\Models\Orders::factory(20)->create();
     }
 }
